49254c55b7a55b73e915bda08e922c46fa215a3a
Rainwhisker <sammyfrw@gmail.com>
2025-06-25 22:29:35 -0600

    testing indentations

--- scripts/incrementZeal.ts
+++ scripts/incrementZeal.ts
@@ -21,37 +21,37 @@ async function incrementZeal() {
 
 function showZealPopup(actorZeal: int): int {
 	new foundry.appv1.api.Dialog({
-  	title: "Award Zeal",
-    content: askZealPopupOption(actorZeal),
-    buttons: {
-      no: {
-	      icon: fontAwesomeIcon("times").outerHTML,
-        label: "Cancel",
-      },
-	    yes: {
-        icon: fontAwesomeIcon("hand").outerHTML,
-        label: "Gain Zeal",
-          callback: ($html, event?: JQuery.TriggeredEvent) => {
-            const html = $html[0];
-            const zeal = Number(html.querySelector<HTMLSelectElement>("[name=zeal]")?.value) ?? 0;
-            return zeal;
-          },
-      },
-    },
-    default: "yes",
-  }).render(true);
+		title: "Award Zeal",
+		content: askZealPopupOption(actorZeal),
+		buttons: {
+			no: {
+				icon: fontAwesomeIcon("times").outerHTML,
+				label: "Cancel",
+			},
+			yes: {
+				icon: fontAwesomeIcon("hand").outerHTML,
+				label: "Gain Zeal",
+					callback: ($html, event?: JQuery.TriggeredEvent) => {
+						const html = $html[0];
+						const zeal = Number(html.querySelector<HTMLSelectElement>("[name=zeal]")?.value) ?? 0;
+						return zeal;
+					},
+			},
+		},
+		default: "yes",
+	}).render(true);
 }
 
 function askZealPopupOption(zeal: int): string {
 	const currentZeal = zeal ?? 0;
 	 return `
-    <form>
-    <div class="form-group">
-        <label>New Zeal Amount</label>
-        <input type="number" name="zeal" value="${currentZeal}">
-    </div>
-    </form>
-    `;
+		<form>
+		<div class="form-group">
+				<label>New Zeal Amount</label>
+				<input type="number" name="zeal" value="${currentZeal}">
+		</div>
+		</form>
+		`;
 }
 
 export {incrementZeal}
