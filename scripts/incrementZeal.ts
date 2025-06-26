6cc00915edbaa2a2430d27e2748afdd9399c19a7
Rainwhisker <sammyfrw@gmail.com>
2025-06-25 22:25:33 -0600

    adjust indentations

--- scripts/incrementZeal.ts
+++ scripts/incrementZeal.ts
@@ -22,25 +22,25 @@ async function incrementZeal() {
 
 function showZealPopup(actorZeal: int): int {
 	new foundry.appv1.api.Dialog({
-        title: "Award Zeal",
-        content: askZealPopupOption(actorZeal),
-        buttons: {
-            no: {
-                icon: fontAwesomeIcon("times").outerHTML,
-                label: "Cancel",
-            },
-            yes: {
-                icon: fontAwesomeIcon("hand").outerHTML,
-                label: "Gain Zeal",
-                callback: ($html, event?: JQuery.TriggeredEvent) => {
-                    const html = $html[0];
-                    const zeal = Number(html.querySelector<HTMLSelectElement>("[name=zeal]")?.value) ?? 0;
-                    return zeal;
-                },
-            },
-        },
-        default: "yes",
-    }).render(true);
+  	title: "Award Zeal",
+    content: askZealPopupOption(actorZeal),
+    buttons: {
+      no: {
+	      icon: fontAwesomeIcon("times").outerHTML,
+        label: "Cancel",
+      },
+	    yes: {
+        icon: fontAwesomeIcon("hand").outerHTML,
+        label: "Gain Zeal",
+          callback: ($html, event?: JQuery.TriggeredEvent) => {
+            const html = $html[0];
+            const zeal = Number(html.querySelector<HTMLSelectElement>("[name=zeal]")?.value) ?? 0;
+            return zeal;
+          },
+      },
+    },
+    default: "yes",
+  }).render(true);
 }
 
 function askZealPopupOption(zeal: int): string {
