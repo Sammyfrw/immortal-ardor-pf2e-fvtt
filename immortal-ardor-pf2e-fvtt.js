a0e9429b480f5562e078800dec41da18f77d1d34
Rainwhisker <sammyfrw@gmail.com>
2025-06-27 11:16:32 -0600

    rename folders to avoid conflicts

--- immortal-ardor-pf2e-fvtt.js
+++ immortal-ardor-pf2e-fvtt.js
@@ -1,4 +1,4 @@
-import { incrementZeal, useZenith } from "./scripts/zealMacros.js";
+import { incrementZeal, useZenith } from "./macros/zealMacros.js";
 
 Hooks.on("init", async () => {
   game.modules.get("immortal-ardor").macros = {
